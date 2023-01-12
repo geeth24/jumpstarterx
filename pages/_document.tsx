a783e19184616405813f4bcb81d723dad7e26315
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-01-12 13:20:23 -0600

    Add SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,11 +10,26 @@ export default class Document extends NextDocument {
         return (
             <Html lang="en">
                 <Head>
-                   
                     <link rel="icon" href="/logo.png" />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Proxima+Nova:wght@300;400;500;600;700&display=swap"
                         rel="stylesheet"
+                    />
+                    <link rel="apple-touch-icon" href="/logo.png" />
+                    <meta property="og:title" content="JumpStarterX" />
+                    <meta
+                        name="image"
+                        property="og:image"
+                        content="/logo.jpg"
+                    />
+                    <meta name="author" content="Geeth Gunnampalli" />
+                    <meta
+                        property="og:description"
+                        content="JumpStarterX is a responsive Next.js application that helps developers or students get a jump start on their projects by providing templated responsive React.js and Chakra UI websites."
+                    />
+                    <meta
+                        property="og:url"
+                        content="https://jumpstarterx.com"
                     />
                 </Head>
                 <body>
