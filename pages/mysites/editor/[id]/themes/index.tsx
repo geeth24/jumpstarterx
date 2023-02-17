4a05b867eebce705276f59b206e5823503fc7bc2
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-02-17 01:21:07 -0600

    Update Theme Picker

--- pages/mysites/editor/[id]/themes/index.tsx
+++ pages/mysites/editor/[id]/themes/index.tsx
@@ -1,5 +1,16 @@
-import { Box, Button, Flex, Heading, Select, VStack } from "@chakra-ui/react"
-import { collection, doc, updateDoc } from "firebase/firestore"
+import {
+    Box,
+    Button,
+    Flex,
+    Grid,
+    GridItem,
+    Heading,
+    HStack,
+    Text,
+    VStack,
+    useColorModeValue,
+} from "@chakra-ui/react"
+import { doc, updateDoc } from "firebase/firestore"
 import Head from "next/head"
 import { useRouter } from "next/router"
 import React from "react"
@@ -70,21 +81,77 @@ function SiteThemes() {
             <Flex as="main" p="4" justifyContent="center">
                 <Box textAlign="center" p="10" maxWidth="7xl">
                     <Heading as="h1" size="2xl" mb="4">
-                        Select a theme
+                        Select A Theme
                     </Heading>
 
                     <VStack spacing="4" align="left">
-                        <Select
-                            placeholder="Select theme"
-                            onChange={(e) => setSelectedTheme(e.target.value)}
-                            value={selectedTheme}
+                        <Grid
+                            templateColumns={{
+                                base: "repeat(2, 1fr)",
+                                md: "repeat(3, 1fr)",
+                                lg: "repeat(4, 1fr)",
+                            }}
+                            gap={6}
                         >
                             {themeOptions.map((theme) => (
-                                <option key={theme} value={theme}>
-                                    {theme}
-                                </option>
+                                <GridItem
+                                    key={theme}
+                                    alignItems="center"
+                                    p="2"
+                                    bg={
+                                        selectedTheme === theme
+                                            ? useColorModeValue(
+                                                  `${theme}.500`,
+                                                  `${theme}.50`
+                                              )
+                                            : useColorModeValue(
+                                                  `${theme}.50`,
+                                                  `${theme}.900`
+                                              )
+                                    }
+                                    cursor="pointer"
+                                    onClick={() => setSelectedTheme(theme)}
+                                    //add inner border
+
+                                    borderRadius="md"
+                                >
+                                    <HStack key={theme}>
+                                        <Box
+                                            w="10"
+                                            h="10"
+                                            bg={
+                                                selectedTheme === theme
+                                                    ? useColorModeValue(
+                                                          `${theme}.300`,
+                                                          `${theme}.500`
+                                                      )
+                                                    : useColorModeValue(
+                                                          `${theme}.500`,
+                                                          `${theme}.500`
+                                                      )
+                                            }
+                                            borderRadius="full"
+                                        />
+                                        <Text
+                                            color={
+                                                selectedTheme === theme
+                                                    ? useColorModeValue(
+                                                          `${theme}.100`,
+                                                          `${theme}.900`
+                                                      )
+                                                    : useColorModeValue(
+                                                          `${theme}.500`,
+                                                          `${theme}.500`
+                                                      )
+                                            }
+                                        >
+                                            {theme.charAt(0).toUpperCase() +
+                                                theme.slice(1)}
+                                        </Text>
+                                    </HStack>
+                                </GridItem>
                             ))}
-                        </Select>
+                        </Grid>
                         <Button
                             onClick={updateTheme}
                             colorScheme={selectedTheme}
